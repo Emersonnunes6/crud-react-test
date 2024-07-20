53bbbdae74ffa76d1319e7340033081daa08de30
emersonnunes6 <enunessena@gmail.com>
2024-07-20 18:48:14 -0300

    integração do msw

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+const deferRender = async () => {
+  const { worker } = await import('./mocks/browser.ts');
+  return worker.start()
+}
+
+deferRender().then(() => {
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+})
+
